7e24cc9ce19a2e6edfdf827d005a4258cc8a4722
Varfolomeev-Ilya <ilya323472@yandex.ru>
2021-01-13 16:53:03 +0300

    create a function randomnumber

--- app.js
+++ app.js
@@ -6,20 +6,39 @@ document.addEventListener('DOMContentLoaded', () => {
   let squares = [];
 
 
-  //playing board
+      //playing board
 
-  function createBoard() {
+    function createBoard() {
       for (let i=0 ; i < width*width; i++) {
         square = document.createElement('div');
         square.innerHTML = 0;
         gridDisplay.appendChild(square);
         squares.push(square);
       }
-  }
-  createBoard()
-  
+      generate()
+      generate()
 
-}) 
+    }
+     createBoard()
 
 
 
+      //random numbers
+
+     function generate() {
+       let randomNumbers = Math.floor(Math.random() * squares.length)
+       if (squares[randomNumbers].innerHTML == 0) {
+          squares[randomNumbers].innerHTML = 2
+       } else generate()
+     } 
+
+
+
+
+
+
+
+
+
+
+})
