c0c121313324af0ab16bb2d0d0d6212e48eb2d73
Varfolomeev-Ilya <ilya323472@yandex.ru>
2021-01-13 11:15:27 +0300

    created a grid playfield, assigned js to search for the elements I needed from html

---
+++ 2048.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMcontentloaded', () => {
+  const gridDisplay = document.querySelector('.grid') ;
+  const scoreDisplay = document.getElementById('score');
+  const resultDisplay = document.getElementById('result');
+}) 
